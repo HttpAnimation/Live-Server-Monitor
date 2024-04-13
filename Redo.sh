a003f862e0926d112756055eaf1b5ce1937f89cd
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-13 11:14:00 -0700

    script to redo

---
+++ Redo.sh
@@ -0,0 +1,5 @@
+# This is only for testing on when am on a VM you can delete this file and be fine
+cd ~/
+git clone -b main https://github.com/HttpAnimation/Live-Server-Monitor.git
+cd Live-Server-Monitor
+echo "Done"
